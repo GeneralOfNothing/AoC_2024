7b1996ac480f1007b71f8923c29c32373299a7c8
Matt <batthew.mond@gmail.com>
2024-12-04 20:04:24 -0800

    Add day 1 and 2 to repo

--- src/Day1/main.cpp
+++ src/Day1/main.cpp
@@ -1,7 +1,63 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<algorithm>
 
 int main(){
-    std::cout << "Hello, from day 1!\n";
+    std::fstream inFile("TestData//01_input.txt");
+
+    std::vector<int> listLeft;
+    std::vector<int> listRight;
+
+    std::string number;
+    int numbersParsed = 0;
+    while(inFile >> number){
+        int swap = numbersParsed % 2;
+        if (swap == 0){
+            listLeft.push_back(stoi(number));
+        }
+        else{
+            listRight.push_back(stoi(number));
+        }
+        numbersParsed++;
+    }
+
+    for (int i = 0; i < listLeft.size(); i++){
+        std::cout << listRight.at(i) << std::endl;
+    }
+
+    std::sort(listLeft.begin(), listLeft.end());
+    std::sort(listRight.begin(), listRight.end());
+
+    //Part One:
+    int difference = 0;
+    if (listLeft.size() == listRight.size()){
+        for (int i = 0; i < listLeft.size(); i++){
+            if (listLeft.at(i) > listRight.at(i)){
+                difference += (listLeft.at(i) - listRight.at(i));
+            }
+            else{
+                difference += (listRight.at(i) - listLeft.at(i));
+            }
+        }
+    }
+
+    std::cout << "Difference score: " << difference << std::endl;
+
+    //Part two:
+    int similarity = 0;
+    for (int i = 0; i < listLeft.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < listRight.size(); j++) {
+            if (listLeft.at(i) == listRight.at(j)) {
+                count++;
+            }
+        }
+        similarity += (listLeft.at(i) * count);
+    }
+
+    std::cout << "Similarity score: " << similarity << std::endl;
 
     return 0;
 }
--- src/Day2/main.cpp
+++ src/Day2/main.cpp
@@ -1,8 +1,129 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
 
-int main(){
+bool isInOrder(std::vector<int> levels);
+bool isGradual(std::vector<int> levels);
+bool isValid(std::vector<int> levels);
+bool isNaturallyValid(std::vector<int> levels);
+int calculateDifference(int numOne, int numTwo);
+
+int main() {
+    std::ifstream inFile("TestData/02_input.txt");
+	std::vector<std::string> reports;
+
+    std::string line;
+    while (!inFile.eof()){
+        while (getline(inFile, line)) {
+            reports.push_back(line);
+        }
+    }
+
+	std::cout << "Number of reports: " << reports.size() << std::endl;
+
+	int safeReports = 0;
+	int naturallySafeReports = 0;
+
+	for (int i = 0; i < reports.size(); i++) {
+		std::vector<int> levels;
+		std::string report = reports.at(i);
+
+		std::string level;
+        for (int j = 0; j < report.size(); j++) {
+			if (isdigit(report.at(j))) {
+				level += report.at(j);
+			}
+            else {
+				levels.push_back(stoi(level));
+				level = "";
+			}
+		}
+		//Consume the final number in level:
+        levels.push_back(stoi(level));
+
+		if (isNaturallyValid(levels)) {
+			naturallySafeReports++;
+		}
+
+		if (isValid(levels)) {
+			safeReports++;
+		}
+	}
+
+	std::cout << "Number of naturally safe reports (Part One): " << naturallySafeReports << std::endl;
+	std::cout << "Number of safe reports (Part Two): " << safeReports << std::endl;
 	
-	std::cout << "This is day two.\n";
-
 	return 0;
+}
+
+bool isNaturallyValid(std::vector<int> levels) {
+
+	if (isInOrder(levels) && isGradual(levels)) {
+		return true;
+	}
+
+	return false;
+}
+
+bool isValid(std::vector<int> levels) {
+	//Part One:
+	if (isInOrder(levels) && isGradual(levels)) {
+		return true;
+	}
+
+	//Part Two: (Allow removal of one element)
+	for (int i = 0; i < levels.size(); i++) {
+		std::vector<int> retry = levels;
+		retry.erase(retry.begin() + i);
+
+		if (isInOrder(retry) && isGradual(retry)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+//Report should either be all increasing or all decreasing
+bool isInOrder(std::vector<int> levels)
+{
+	bool isIncreasing = true;
+
+	if (levels.at(0) > levels.at(1)) {
+		isIncreasing = false;
+	}
+
+	switch (isIncreasing) {
+	case true:
+		for (int i = 0; i < levels.size() - 1; i++) {
+			if (levels.at(i) >= levels.at(i + 1)) {
+				return false;
+			}
+		}
+		return true;
+	case false:
+		for (int i = 0; i < levels.size() - 1; i++) {
+			if (levels.at(i) <= levels.at(i + 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+//Any two adjacent levels should differ by at least one and at most three.
+bool isGradual(std::vector<int> levels)
+{
+	for (int i = 0; i < levels.size() - 1; i++) {
+		int diff = calculateDifference(levels.at(i), levels.at(i + 1));
+		if (diff > 3 || diff < 1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int calculateDifference(int numOne, int numTwo) {
+	return abs(numOne - numTwo);
 }
